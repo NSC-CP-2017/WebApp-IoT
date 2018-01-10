cec5fe309c02be82a9a820f182ad68701cfb67e6
Gpanayu <gpanayu@yahoo.co.th>
2018-01-10 14:57:22 +0700

    comment cron in webapp

--- weatherCronjob.js
+++ weatherCronjob.js
@@ -2,13 +2,14 @@ var request = require('request');
 var cronjob = require('cron').CronJob;
 
 module.exports = function(){
-	var job = new cronjob('00 00 */3 * * *', function() {
-		request({
-			uri:"http://localhost:3352/fetchweather",
-			method: "get",
-		}, function(error, response, body) {
-		console.log('notification update at : '+Date());
-		});
+	var job = new cronjob('0 0 */3 * * *', function() {
+		console.log("update weather at "+new Date());
+		// request({
+		// 	uri:"http://localhost:3352/fetchweather",
+		// 	method: "get",
+		// }, function(error, response, body) {
+		// console.log('notification update at : '+Date());
+		// });
 	}, null, true, 'Asia/Bangkok');
 	return job;
 }
