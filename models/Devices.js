7f956f51f4175a77715ea002ce0db96868ff030a
kamemos <mosagile@gmail.com>
2018-01-28 01:14:52 +0700

    add riskRule to device

--- models/Devices.js
+++ models/Devices.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var schemaDevices = new Schema({
-<<<<<<< HEAD
     name : String,
     owner : Schema.ObjectId,
     deviceID: String,
@@ -19,23 +18,5 @@ var schemaDevices = new Schema({
     data : Object,
     desc : String,
     settings : Object
-=======
-  name: String,
-  owner: Schema.ObjectId,
-  deviceID: String,
-  deviceKey: String,
-  deviceSecret: String,
-  online: Boolean,
-  lastOnline: Date,
-  weather: Object,
-  forecastWeather: Object,
-  features: Array,
-  lastUpdateWeather: Date,
-  lastUpdateFeatures: Date,
-  data: Object,
-  desc: String,
-  settings: Object,
-  riskRule: Object
->>>>>>> origin/master
 });
 module.exports = mongoose.model('devices', schemaDevices);
