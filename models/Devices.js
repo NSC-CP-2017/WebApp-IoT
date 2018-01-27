f80586a45507430918d6dabe3a82cbf8fe4028a5
aroundy <gam_wannita@hotmail.com>
2018-01-27 22:38:36 +0700

    fix delete device/add tooltip

--- models/Devices.js
+++ models/Devices.js
@@ -1,21 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var schemaDevices = new Schema({
-    name : String,
-    owner : Schema.ObjectId,
-    deviceID: String,
-    deviceKey: String,
-    deviceSecret: String,
-    online: Boolean,
-    lastOnline: Date,
-    weather: Object,
-    forecastWeather: Object,
-    features : Array,
-    lastUpdateWeather : Date,
-    lastUpdateFeatures : Date,
-    data : Object,
-    desc : String,
-    settings : Object
+  name: String,
+  owner: Schema.ObjectId,
+  deviceID: String,
+  deviceKey: String,
+  deviceSecret: String,
+  online: Boolean,
+  lastOnline: Date,
+  weather: Object,
+  forecastWeather: Object,
+  features: Array,
+  lastUpdateWeather: Date,
+  lastUpdateFeatures: Date,
+  data: Object,
+  desc: String,
+  settings: Object,
+  riskRule: Object
 });
-module.exports = mongoose.model('devices',schemaDevices);
-
+module.exports = mongoose.model('devices', schemaDevices);
